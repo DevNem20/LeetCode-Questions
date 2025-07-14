02a28e4e33e55130bae218f011611e316516199a
DevNem20 <162136859+DevNem20@users.noreply.github.com>
2025-07-14 21:18:13 +0530

    Time: 4 ms (57.09%) | Memory: 14.9 MB (36.89%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -4,15 +4,15 @@ public:
         int n = nums.size();
         vector<int> result;
         unordered_map<int, int> mpp;
-        for(int i = 0; i<n; i++) {
-            mpp[nums[i]] = i;
-        }
+
+       
 
         for(int i = 0; i<n; i++) {
-            for(int j = i+1; j <n; j++) {
-                if(nums[i] + nums[j] == target) return {i,j};
-            }
+            int moreNeeded = target - nums[i];
+            if(mpp.count(moreNeeded)) return {mpp[moreNeeded], i};
+            mpp[nums[i]] = i;
         }
-        return {};
+        return {-1,-1};
+
     }
 };
