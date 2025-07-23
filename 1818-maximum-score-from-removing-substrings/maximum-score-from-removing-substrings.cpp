27873c4faca62d4f10d42130f0d9bc181967f2ab
DevNem20 <162136859+DevNem20@users.noreply.github.com>
2025-07-23 20:07:05 +0530

    Time: 55 ms (50.75%) | Memory: 31.8 MB (20.77%) - LeetSync

---
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+
+    string removeSubstr(string& s, string& matchStr) {
+        stack<char> st;
+
+        for(char& ch: s) {
+            if(ch == matchStr[1] && !st.empty() && st.top() == matchStr[0]) {
+                st.pop();
+            }
+            else{
+                st.push(ch);
+            }
+        }
+
+        string result;
+        while(!st.empty()) {
+            result.push_back(st.top());
+            st.pop();
+        }
+        reverse(begin(result), end(result));
+        return result;
+    }
+    int maximumGain(string s, int x, int y) {
+        int n     = s.length();
+        int score = 0;
+
+        string maxStr = (x >= y) ? "ab" : "ba";
+        string minStr = (x < y) ? "ab" : "ba";
+
+        string temp_first = removeSubstr(s, maxStr);
+        int L = temp_first.length();
+
+        int charRemoved = (n - L);
+        score += (charRemoved/2) * max(x, y);
+
+
+        string temp_second = removeSubstr(temp_first, minStr);
+        charRemoved = L - temp_second.length();
+        score += (charRemoved/2) * min(x, y);
+
+        return score;
+    }
+};
